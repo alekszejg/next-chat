b4a5dff44f30c42796ef30caa116701da9b555cf
Alekszej Guljajev <alexeyguljajev@gmail.com>
2024-09-29 19:56:42 +0200

    First commit. Did basic tailwind configuration, got fonts, improved folder structure and removed default stuff

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,18 +1,18 @@
 import type { Config } from "tailwindcss";
 
 const config: Config = {
-  content: [
-    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",
-  ],
+  content: ["./app/**/*.{js,ts,jsx,tsx,mdx}"],
   theme: {
-    extend: {
-      colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
-      },
+    fontFamily: {
+      'source-sans3-regular': ['Source-Sans3-Regular', 'sans-serif'],
+      'source-sans3-medium': ['Source-Sans3-Medium', 'sans-serif'],
+      'source-sans3-bold': ['Source-Sans3-Bold', 'sans-serif'], 
     },
+    screens: {
+      'tablet': '480px',
+      'ultrawide': '1400px',
+    },
+    extend: {},
   },
   plugins: [],
 };

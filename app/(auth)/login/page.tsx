22d68b7121ab769733f52ea010e5b1d2d29d5e00
Alekszej Guljajev <alexeyguljajev@gmail.com>
2024-09-30 21:10:47 +0200

    No idea why it's modified

--- app/(auth)/login/page.tsx
+++ app/(auth)/login/page.tsx
@@ -5,6 +5,8 @@ export const metadata: Metadata = {
     title: "Login - NextChat"
 };
 
+export type FormInputs = {email: string, password: string};
+
 
 export default function LoginPage() {
     return <AuthPage action="login" />;

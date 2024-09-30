7b68c50dbab07d421db86bdaf3ff6cd26d33d445
Alekszej Guljajev <alexeyguljajev@gmail.com>
2024-09-30 05:59:31 +0200

    Sidebar is almost fully functional (theme cant be changed yet). Had to change to client side since pathname can only be obtained on client-interactions to apply dynamic tailwind classes

--- app/_layoutComponents/sidebar.tsx
+++ app/_layoutComponents/sidebar.tsx
@@ -1,26 +1,32 @@
+"use client";
 import Link from "next/link";
 import Image from "next/image";
-import routes from "@/app/allRoutes.json";
-import { Sun, UserRoundSearch, MessageSquare, Phone, Settings } from "lucide-react";
+import { usePathname } from "next/navigation";
+import { Sun } from "lucide-react";
+import { routes } from "./sidebar.data";
+
 
 export default function Sidebar() {
     const styling = {
         aside: "flex flex-col justify-between items-center h-full py-6 border-r-2 border-borderWhite",
         buttonMenu: "flex flex-col items-center gap-y-8",
-        icon: "text-[hsl(249,4%,50%)] hover:text-[hsl(249,4%,100%)]"
+        icon: "text-[hsl(249,4%,50%)] hover:text-[hsl(249,4%,100%)]",
+        activeIcon: "text-[hsl(249,4%,100%)]"
     }
+
+    const currentPath = usePathname();
 
     return (
         <aside className={styling.aside}>
              <Sun className={styling.icon} />
              <div className={styling.buttonMenu}>
-                <Link href={routes.features.friends}><UserRoundSearch className={styling.icon}/></Link>
-                <Link href={routes.features.chats}><MessageSquare className={styling.icon} /></Link>
-                <Link href={routes.features.calls}><Phone className={styling.icon} /></Link>
-                <Link href={routes.features.settings}><Settings className={styling.icon} /></Link>
+                {routes.map(route => (
+                    <Link href={route.url} key={route.title}>
+                        <route.icon className={currentPath === route.url ? styling.activeIcon : styling.icon} />
+                    </Link>
+                ))}
              </div>
             <Image width={32} height={32} src="/icons/nextChatLogo.svg" alt="logo" priority/>
         </aside>
-    
     );
 }

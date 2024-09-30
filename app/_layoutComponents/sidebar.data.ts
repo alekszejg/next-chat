c90f39d0e8e0297db23b9f7e58ec6207f6ec30fb
Alekszej Guljajev <alexeyguljajev@gmail.com>
2024-09-30 05:57:44 +0200

    Removed routes from json file and placed them into typescript file, which allows Icons to be added into array of objects that represent routes

---
+++ app/_layoutComponents/sidebar.data.ts
@@ -0,0 +1,24 @@
+import { UserRoundSearch, MessageSquare, Phone, Settings } from "lucide-react";
+
+export const routes = [
+    {
+        title: "friends",
+        url: "/friends",
+        icon: UserRoundSearch,
+    },
+    {
+        title: "chats",
+        url: "/chats",
+        icon: MessageSquare,
+    },
+    {
+        title: "calls",
+        url: "/calls",
+        icon: Phone,
+    },
+    {
+        title: "settings",
+        url: "/settings",
+        icon: Settings
+    }
+]

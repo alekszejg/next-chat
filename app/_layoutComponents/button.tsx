405c9456ae1ea16a91807df9bd24aa08c75e7f91
Alekszej Guljajev <alexeyguljajev@gmail.com>
2024-10-02 00:28:18 +0200

    Added disabled attrbitute to button based on incoming state boolean

--- app/_layoutComponents/button.tsx
+++ app/_layoutComponents/button.tsx
@@ -1,14 +1,15 @@
 import { LoaderCircle } from "lucide-react";
 
 
-export default function Button(props: {text: string, className: string, isLoading: boolean}) {
-    const { text, className, isLoading } = props;
+export default function Button(props: {text: string, className: string, isLoading: boolean, disabled?: boolean}) {
+    const { text, className, isLoading, disabled } = props;
+    
     const styling = {
         loader: "mx-auto animate-spin"
     };
 
     return (
-        <button className={className}>
+        <button className={className} disabled={disabled || isLoading}>
             {isLoading && <LoaderCircle className={styling.loader} />}
             {!isLoading && text}
         </button>

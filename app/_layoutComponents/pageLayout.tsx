3aafd93c5dd9625bd7945fe43c40b7304ffe9130
Alekszej Guljajev <alexeyguljajev@gmail.com>
2024-10-02 00:26:30 +0200

    Added react-hook-toast to receive and show login/registration errors

--- app/_layoutComponents/pageLayout.tsx
+++ app/_layoutComponents/pageLayout.tsx
@@ -1,15 +1,19 @@
-import { ReactNode } from "react";
+import { Toaster } from "react-hot-toast";
 import Sidebar from "./sidebar";
 
-export default function PageLayout(props: {children: ReactNode}) {
+
+export default function PageLayout(props: {children: React.ReactNode}) {
     const styling = {
-        main: "grid grid-cols-clientLayout h-full"
+        main: "grid grid-cols-clientLayout h-full",
+        errorToaster: "w-[100px] bg-white"
+    
     };
 
     return (
         <main className={styling.main}>
             <Sidebar />
             {props.children}
+            <Toaster position="top-right" />
         </main>
     );
 }

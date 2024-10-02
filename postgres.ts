dc6ffae3122cafb33d0331b9018b131ce8a5c9dc
Alekszej Guljajev <alexeyguljajev@gmail.com>
2024-10-02 17:56:23 +0200

    added 'pg' package to work with postgres, along with dotenv and tsx packages to work with postgres.ts file on its own for debugging. Later remove those 2 dependencies

---
+++ postgres.ts
@@ -0,0 +1,12 @@
+import { Pool } from 'pg';
+import dotenv from 'dotenv';
+dotenv.config();
+
+
+export let pool = new Pool({
+  user: process.env.USER,
+  host: process.env.HOST,
+  database: process.env.DATABASE,
+  password: process.env.PASSWORD,
+  port: Number(process.env.DB_PORT),
+});

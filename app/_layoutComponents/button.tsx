757a3059e9690a3016f259c4be93f08e7eb0cc7c
Alekszej Guljajev <alexeyguljajev@gmail.com>
2024-10-03 05:33:38 +0200

    Added type='submit' and type='button' to custom button

--- app/_layoutComponents/button.tsx
+++ app/_layoutComponents/button.tsx
@@ -1,15 +1,22 @@
 import { LoaderCircle } from "lucide-react";
 
+type CustomButton = {
+    text: string, 
+    className: string, 
+    isLoading: boolean, 
+    type: "submit" | "button", 
+    disabled?: boolean
+};
 
-export default function Button(props: {text: string, className: string, isLoading: boolean, disabled?: boolean}) {
-    const { text, className, isLoading, disabled } = props;
+export default function Button(props: CustomButton) {
+    const { text, className, isLoading, type, disabled } = props;
     
     const styling = {
         loader: "mx-auto animate-spin"
     };
 
     return (
-        <button className={className} disabled={disabled || isLoading}>
+        <button className={className} type={type} disabled={disabled || isLoading}>
             {isLoading && <LoaderCircle className={styling.loader} />}
             {!isLoading && text}
         </button>

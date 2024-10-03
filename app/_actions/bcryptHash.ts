20bcef6fad8ff565b64a685a36250bd39a8839bd
Alekszej Guljajev <alexeyguljajev@gmail.com>
2024-10-03 08:05:09 +0200

    Added a folder for server actions, which will be handling input validation, password hashes etc...

---
+++ app/_actions/bcryptHash.ts
@@ -0,0 +1,16 @@
+"use server";
+import bcrypt from 'bcrypt';
+
+// Hash password
+export async function hashPassword(password: string, saltRounds: number = 10) {
+    const hashedPassword = await bcrypt.hash(password, saltRounds);
+    return hashedPassword;
+}
+
+
+// Compare string input with hashed value
+export async function comparePasswordHash(stringPassword: string, hashedPassword: string) {
+    const isMatch = bcrypt.compare(stringPassword, hashedPassword);
+    return isMatch;
+}
+

f95dc9ee370645bdf83804404cc04eb686d40fd0
Alekszej Guljajev <alexeyguljajev@gmail.com>
2024-10-03 03:00:14 +0200

    Added a function to connect and disconnect the db client/pool, saves me hours having to write same connect/disconnect code

--- postgres.ts
+++ postgres.ts
@@ -1,4 +1,5 @@
 import { Pool } from 'pg';
+import type { PoolClient } from 'pg';
 import dotenv from 'dotenv';
 dotenv.config();
 
@@ -10,3 +11,24 @@ export let pool = new Pool({
   password: process.env.PASSWORD,
   port: Number(process.env.DB_PORT),
 });
+
+
+export default async function ConnectPgsqlPool(action: "connect" | "disconnect", client: PoolClient | null = null, ) {
+    
+  if (action === "connect") {
+      try {
+          client = await pool.connect();
+          return client;
+      }
+      catch {
+          console.error("Couldn't connect to database");
+          return null;
+      }
+  }
+
+  else if (client && action === "disconnect") {
+      client.release();
+  }
+
+  return null;
+}

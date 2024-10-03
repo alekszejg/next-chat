c7f269fade51b7d73c04218f00626482fefcb206
Alekszej Guljajev <alexeyguljajev@gmail.com>
2024-10-03 20:35:48 +0200

    Reduced code for input sanitization function, tried few things warguments passed from password verifier (since it involved having special characters)

--- app/_actions/handleRawInput.ts
+++ app/_actions/handleRawInput.ts
@@ -1,25 +1,25 @@
 "use server";
 import sanitize from "sanitize-html"
 
+
 export async function sanitizeInput(rawInput: string | string[]) {
-    let cleanInput: string | string[]; // array is for form values
-  
-    if (typeof rawInput === "string") {
-        cleanInput = sanitize(rawInput);
-    } else {
-        cleanInput = rawInput.map(rawString => sanitize(rawString));
-    }
     
-    return cleanInput;
+    if (typeof rawInput === "string") return sanitize(rawInput);
+
+    else {
+        const cleanInput = rawInput.map(rawString => sanitize(rawString));
+        return cleanInput;
+    }
 }
 
 export async function escapeInput(rawInput: string) {
    const replaceKeys: Record<string, string> = {"<": "&lt;", ">": "&gt;", "&": "&amp;", '"': "&quot;", "'": "&#x27;"};
    
    const cleanInput = Object.keys(replaceKeys).reduce((acc, key) => {
-        const globalMatch = new RegExp(key, 'g'); // Find all occurances of each key
-        return acc.replace(globalMatch, replaceKeys[key]); // Accumulates all .replace() changes throughout all iterations
+        const globalMatch = new RegExp(key, 'g'); 
+        return acc.replace(globalMatch, replaceKeys[key]); 
     }, rawInput);
     
     return cleanInput;
 }
+

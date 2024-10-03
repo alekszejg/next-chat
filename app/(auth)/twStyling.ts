2ab9c58a56d99d846512e98e633f34be7d1dd299
Alekszej Guljajev <alexeyguljajev@gmail.com>
2024-10-03 20:24:12 +0200

    Since tailwind styling is shared I have placed it into separate file

---
+++ app/(auth)/twStyling.ts
@@ -0,0 +1,18 @@
+const styling = {
+    form: {
+        form: "flex justify-center items-center",
+        contentWrapper: "flex flex-col gap-y-5",
+        header: "mx-auto text-3xl",
+        button: "self-center w-3/4 h-[2rem] border-2 border-[hsl(249,3%,60%)] rounded-lg",
+        alreadyRegisteredText: "text-[hsl(249,0%,61%)]",
+        loginLink: "ml-3 text-[hsl(249,0%,90%)] hover:underline"
+    },
+    input: {
+        wrapper: "flex flex-row items-center",
+        icon: "inline-block w-[2rem] mr-1.5",
+        input: "h-[2rem] px-1.5",
+        error: ""
+    }
+};
+
+export default styling;

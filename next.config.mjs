5d1df4ae2f8cd345e5ccb899788f43a8d1ef9ef0
Alekszej Guljajev <alexeyguljajev@gmail.com>
2024-10-04 22:44:36 +0200

    Added CORS anonymous which I heard does add a bit of security against malicious domains but still need to research how to prevent requests to public api routes by some bots

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    reactStrictMode: false
+    reactStrictMode: false,
+    crossOrigin: 'anonymous'
 };
 
 export default nextConfig;

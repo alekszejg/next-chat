489c78dba75ebea9feaccc034277a135cee0edca
Alekszej Guljajev <alexeyguljajev@gmail.com>
2024-10-04 22:46:05 +0200

    Added 404 error when requesting non existant api route for testing. Still have to prevent my public API from spam requests

--- middleware.ts
+++ middleware.ts
@@ -1,9 +1,35 @@
+import type { NextRequest } from 'next/server';
+import { NextResponse } from 'next/server';
 import { withAuth } from 'next-auth/middleware';
 
-export default withAuth({
+export default function middleware(req: NextRequest) {
+    const url = req.nextUrl;
+    const pathname = url.pathname;
+    const queryParams = url.searchParams;
+
+    if (pathname === "/api/user") {
+        const emailParam = queryParams.get("email");
+        const usernameParam = queryParams.get("username");
+
+        if (!emailParam && !usernameParam) {
+            return NextResponse.json({ error: 'Invalid Route' }, { status: 404 });
+        }
+    }
+
+    return NextResponse.next();
+}
+
+
+export const checkAuth = withAuth({
     pages: {
         signIn: '/login', 
     },
 });
 
-export const config = { matcher: ["/calls", "/chats", "/friends", "/settings"]};
+
+export const config = {
+    matcher: [
+        "/register", "/login", "/calls", "/chats", "/friends", "/settings",
+        "/api/user"
+    ]
+};

0cd1699b81eff74b89d26b5d9246afb55b187e9f
Alekszej Guljajev <alexeyguljajev@gmail.com>
2024-10-05 03:06:49 +0200

    Decided to remove fetchclient instance to avoid performing additional async/await actions and possibly reduce possibility of unexpected errors. The code of GET route is clear but could use some custom error handler later

--- app/api/user/route.ts
+++ app/api/user/route.ts
@@ -1,6 +1,7 @@
 import type { NextRequest } from 'next/server';
 import { NextResponse } from 'next/server';
 import ConnectPgsqlPool from '@/postgres';
+import { PoolClient } from 'pg';
 
 
 export async function GET(req: NextRequest) {
@@ -8,45 +9,45 @@ export async function GET(req: NextRequest) {
     const email = searchParams.get('email');
     const username = searchParams.get('username');
 
-    if (!searchParams || !email && !username) {
-        const error = "Incorrect query parameters";
-        return NextResponse.json({error: error}, {status: 500});
+    if (!email && !username) {
+        const error = "Invalid query parameters";
+        return NextResponse.json({error: error}, {status: 400});
+    }
+
+    let client: PoolClient | null = null;
+
+    try {
+        client = await ConnectPgsqlPool("connect");
+        if (!client) {
+            console.error("Returned PoolClient is null at GET /api/user");
+            return NextResponse.json({error: "Internal Error"}, {status: 500});
+        }
+    } 
+
+    catch (error) {
+        console.error("Failed to reach ConnectPgsqlPool() function at GET /api/user");
+        return NextResponse.json({error: "Internal Error"}, {status: 500});
     }
 
     try {
-        const client = await ConnectPgsqlPool("connect");
-        
-        if (!client) {
-            const error = "Connection to database failed";
-            return NextResponse.json({error: error}, {status: 500});
-        }
-
-        try {
-            let query: string;
-            let queryParams: string[];
-            
-            if (email) {
-                query = 'SELECT * FROM users WHERE email = $1';
-                queryParams = [email];
-            } else if (username) {
-                query = 'SELECT * FROM users WHERE username = $1';
-                queryParams = [username];
-            } else {
-                const error = "Incorrect query parameters";
-                return NextResponse.json({error: error}, {status: 500});
-            }
-            
-            const response = await client.query(query, queryParams);
-            ConnectPgsqlPool("disconnect", client);
+        if (email) {
+            const query = 'SELECT * FROM users WHERE email = $1';
+            const response = await client.query(query, [email]);
             return NextResponse.json({userExists: response.rows.length > 0}, {status: 200});
+        } 
+        else if (username) {
+            const query = 'SELECT * FROM users WHERE username = $1';
+            const response = await client.query(query, [username]);
+            return NextResponse.json({userExists: response.rows.length > 0}, {status: 200});
+        } 
+        else {
+            console.error("Unexpected error at GET /api/user db query, when either email or username passed checks");
+            return NextResponse.json({error: "Invalid query parameters."}, {status: 400});
         }
-        catch {
-            const error = "Database query has failed";
-            return NextResponse.json({error: error}, {status: 500});
-        }
-    }
-    catch {
-        const error = "Failed to reach ConnectPgsqlPool() function";
-        return NextResponse.json({error: error}, {status: 500});
     } 
+    
+    catch {
+        console.error("Database query failed at GET /api/user db query");
+        return NextResponse.json({error: "Internal Error"}, {status: 500});
+    }
 }

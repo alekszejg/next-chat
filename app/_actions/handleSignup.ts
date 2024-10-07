fb1fe82f14a97a66c8c909f5acb442561b2ba1c6
Alekszej Guljajev <alexeyguljajev@gmail.com>
2024-10-07 21:08:37 +0200

    Removed POST request from route.ts since server actions are safer for that. Finished handlesignup action so it returned an object instead of json (error occurs otherwise)

--- app/_actions/handleSignup.ts
+++ app/_actions/handleSignup.ts
@@ -1,106 +1,91 @@
 "use server";
-import { NextResponse } from 'next/server';
-import { sanitizeInput } from "@/app/_actions/handleRawInput";
+import { sanitizeInput } from "@/app/_utils/handleRawInput";
 import { hashPassword } from '@/app/_actions/bcryptHash';
-import type { SignupFormInputs } from "@/app/(auth)/register/page";
+import ConnectPgsqlPool from '@/postgres';
+import { PoolClient } from 'pg';
+import { FieldValues } from 'react-hook-form';
 
 
-// HTTP 400: badInput, HTTP 409: emailExists
-const clientErrors = {
-    badInput: {
-        name: {status: false, message: "Name contains illegal characters"},
-        email: {status: false, message: "Email contains illegal characters"},
-        password: {status: false, message: "Password contains illegal characters"},
-    },
-    emailExists: {status: false, message: "Email already exists. Add another one or login instead"},
+export type SignupResponse = {
+    submitted: boolean,
+    badInput: {name: boolean, email: boolean, password: boolean},
+    badInputExists: boolean
+    emailExists: boolean,
+    internal: boolean
+}
+
+const signupResponse: SignupResponse = {
+    submitted: false,
+    badInput: {name: false, email: false, password: false},
+    badInputExists: false,
+    emailExists: false,
+    internal: false
 };
 
-// HTTP 500 
-const serverError = {message: "Internal error has occured. Try again later"};
 
-
-export default async function handleSignup(rawData: SignupFormInputs) {
+export default async function handleSignup(rawData: FieldValues): Promise<SignupResponse> {
     const { name, email, password } = rawData;
     
-    // error states
-    let badInput = false;
-    let emailExists= false;
-    let internalError = false;
-
-    // scope change for another try block
-    let safeName: string | undefined;
-    let safeEmail: string | undefined;
-    let safePassword: string | undefined; 
-
+    const [cleanName, cleanEmail, cleanPassword] = sanitizeInput([name, email, password]);
+    
+    if (!cleanName || !cleanEmail || !cleanPassword) {
+        signupResponse.badInputExists = true;
+        if (!cleanName) signupResponse.badInput.name = true;
+        if (!cleanEmail) signupResponse.badInput.email = true;
+        if (!cleanPassword) signupResponse.badInput.password = true;
+    }
+    
+    if (!cleanEmail) return signupResponse; // Can't verify email if input is falsy
+      
+    // Verify email uniqueness
     try {
-        // Sanitize inputs
-        const [cleanName, cleanEmail, cleanPassword] = await sanitizeInput([name, email, password])
-        if (!cleanName || !cleanEmail || !cleanPassword) {
-            badInput = true;
-            !cleanName && (clientErrors.badInput.name.status = true);
-            !cleanEmail && (clientErrors.badInput.email.status = true);
-            !cleanPassword && (clientErrors.badInput.password.status = true);
+        const url = process.env.DOMAIN + `/api/user?email=${cleanEmail}`
+        const response = await fetch(url, {method: "GET", headers: {'Content-Type': 'application/json'}});
+        const data = await response.json();
+        
+        if (response.ok && data.userExists) {
+            signupResponse.emailExists = true
+            return signupResponse;
+        } 
+        else if (signupResponse.badInputExists) return signupResponse;
+      
+        else if (!response.ok) {
+            console.error("Unexpected error: either during data fetching from GET /api/user?email or it was accessed with invalid parameters from handleSignup() action");
+            return signupResponse;
+        }
+    } 
+    
+    catch {
+        console.error("Either sanitizeInput() action call or fetching GET /api/user?email failed in handleSignup() action");
+        signupResponse.internal = true;
+        return signupResponse;
+    }
+    
+    
+    // Hash password and POST data
+    let client: PoolClient | null = null;
+    try {
+        const hashedPassword = await hashPassword(cleanPassword as string);
+        client = await ConnectPgsqlPool("connect");
+        
+        if (!client) {
+            console.error("Returned PoolClient is null at GET /api/user");
+            signupResponse.internal = true;
+            return signupResponse;
         }
 
-        // scope change for another try block
-        cleanName && (safeName = cleanName);
-        cleanEmail && (safeEmail = cleanEmail);
-        cleanPassword && (safePassword = cleanPassword);
-
-        // Ensure email is unique
-        if (cleanEmail) {
-            const url = process.env.DOMAIN + `/api/user?email=${email}`
-            const response = await fetch(url, {
-                method: "GET",
-                headers: {'Content-Type': 'application/json'},
-            });
-            
-            const data = await response.json();
-
-            switch (response.status) {
-                case 200: 
-                    if (data.userExists) {
-                        emailExists = true;
-                        clientErrors.emailExists.status = true;
-                        return NextResponse.json(clientErrors, {status: 400});
-                    }
-                    break;
-
-                case 400:
-                    console.error("Unexpected error for GET /api/user?email accessed with invalid parameters from handleSignup() action");
-                case 500:
-                    console.error("Internal db related error when fetching GET /api/user?email from handleSignup() action")
-                
-                default:
-                    internalError = true;
-                    return NextResponse.json(clientErrors, {status: 400}); 
-            }
-        }
+        const query = 'INSERT INTO users (name, email, password) VALUES ($1, $2, $3)';
+        await client.query(query, [cleanName, cleanEmail, hashedPassword]);
+        ConnectPgsqlPool("disconnect", client);
+        signupResponse.submitted = true
+        return signupResponse;
     }
 
     catch {
-        internalError = true;
-        console.error("Either sanitizeInput() action call or fetching GET /api/user?email failed in handleSignup() action");
-        NextResponse.json(serverError, {status: 500});
-    }
-    
-
-    if (badInput || emailExists) {
-        return NextResponse.json(clientErrors, {status: 400});
-    } else if (internalError) {
-        return NextResponse.json(serverError, {status: 500});
-    }
-
-    // proceed with hashing password when no more client errors left
-    else {
-        try {
-            const hashedPassword = await hashPassword(safePassword as string);
-        }
-
-        catch {
-            console.error("Failed to reach hashPassword() action or add data to DB");
-            NextResponse.json(serverError, {status: 500});
-        }
+        client && ConnectPgsqlPool("disconnect", client);
+        console.error("Failed to reach hashPassword() action or add data to DB");
+        signupResponse.internal = true;
+        return signupResponse;
     }
 }
 

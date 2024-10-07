23e3952b9c91a28a96d0d210a36a66264bd4d8f7
Alekszej Guljajev <alexeyguljajev@gmail.com>
2024-10-07 21:10:43 +0200

    Removed separate component for password requirements and it is now handled by react hook form. Still need to check how to od both onChange and onblur validation for same field. Also adapted registration page to handle response from handlesignup action

--- app/(auth)/register/page.tsx
+++ app/(auth)/register/page.tsx
@@ -1,45 +1,73 @@
 "use client";
 import Link from "next/link";
-import { useState, useEffect } from "react";
+import { useState } from "react";
 import { useForm, SubmitHandler } from "react-hook-form";
+import type { FieldValues } from "react-hook-form";
+import { useRouter } from "next/navigation";
 import toast from "react-hot-toast";
 import { CircleUserRound, Mail, Repeat, Lock} from "lucide-react";
-import PasswordChecklist from "./passwordRequirements";
 import InputField from "@/app/(auth)/_components/inputField";
 import Button from "@/app/_layoutComponents/button";
 import SignInWithGoogleButton from "@/app/(auth)/_components/signInGoogleButton";
 import styling from "@/app/(auth)/twStyling";
 import prepSignupData from "@/app/_actions/handleSignup";
-
+import type { SignupResponse } from "@/app/_actions/handleSignup";
 
 export type SignupFormInputs = {name: string, email: string, password: string, reEmail: string};
 
 
 export default function RegistrationPage() {
+    const router = useRouter();
+
     const { 
         register, 
         handleSubmit, 
         reset, 
         getValues, 
         formState: { errors, isSubmitting } 
-    } = useForm<SignupFormInputs>({mode: "onBlur"});
+    } = useForm<SignupFormInputs>({mode: "onChange"});
     
-
-    const [password, setPassword] = useState("");
-    const [passwordPopup, showPasswordPopup] = useState(false);
-
-
-    const onSubmit: SubmitHandler<SignupFormInputs> = async (data) => {
-        const response = await prepSignupData(data)
+    const [submitErrors, setSubmitErrors] = useState({
+        nameInput: "", 
+        emailInput: "", 
+        passwordInput: "",
+        emailExists: "",
+        internal: ""  
+    });
+    
+    
+    const onSubmit = async (formInputs: FieldValues) => {
+        const response = await prepSignupData(formInputs);
         
-        if (!response) {
-            toast.error("Authentication failed");
-            return "Authentication Error: couldn't verify your credentials. Please try again";
+        if (response.submitted) {
+            toast.success("Registration Successful");
+            reset();
+            setTimeout(() => {
+                router.push("login");
+            }, 1000);
         } 
+        
+        else if (!response.internal && response.badInputExists || response.emailExists) {
+            toast.error("Invalid information provided");
+            setSubmitErrors({
+                nameInput: response.badInput.name ? "Name contains illegal characters" : "",
+                emailInput: response.badInput.email ? "Email contains illegal characters": "",
+                passwordInput: response.badInput.password? "Password contains illegal characters": "",
+                emailExists: response.emailExists ? "Email already exists. Add another one or login instead": "",
+                internal: ""
+            });
+        }
+
+        else if (response.internal) {
+            toast.error("Internal error has occured");
+            setSubmitErrors(prev => ({
+                ...prev, internal: "Internal error has occured. Please try again later"
+            }));
+        }        
     }
 
     return (
-        <form method="post" onSubmit={handleSubmit((data) => onSubmit(data))} className={styling.form.form}>
+        <form method="post" onSubmit={handleSubmit(onSubmit)} className={styling.form.form}>
 
             <div className={styling.form.contentWrapper}>
                 <h2 className={styling.form.header}>Register</h2>
@@ -47,6 +75,7 @@ export default function RegistrationPage() {
                 <InputField placeholder="Name:" type="text" Icon={CircleUserRound} styling={styling.input} 
                 {...register('name', {required: "Name is required"})}/>
                 {errors.name && <p className={styling.input.error}>{errors.name.message}</p>}
+                {submitErrors.nameInput && <p className={styling.input.error}>{submitErrors.nameInput}</p>}
                 
                 <InputField placeholder="Enter email:" type="text" Icon={Mail} styling={styling.input} 
                     {...register('email', {
@@ -60,13 +89,20 @@ export default function RegistrationPage() {
                     })}
                 /> 
                 {errors.email && <p className={styling.input.error}>{errors.email.message}</p>}
-                       
+                {submitErrors.emailInput && <p className={styling.input.error}>{submitErrors.emailInput}</p>}
+                {submitErrors.emailExists && <p className={styling.input.error}>{submitErrors.emailExists}</p>}
+
                 <InputField placeholder="Repeat email:" type="text" Icon={Repeat} styling={styling.input} 
                     {...register('reEmail', {
                         required: "Email is required", 
                         validate: (value) => {
                             if (value !== getValues('email')) {
                                 return "Confirmation email must match original email";
+                            } else return true;
+                        },
+                        onBlur: (e: React.FocusEvent<HTMLInputElement>) => {
+                            if (e.target.value !== getValues('email')) {
+                                return "Confirmation email must match original email"
                             } else return true;
                         }
                     })}
@@ -76,33 +112,27 @@ export default function RegistrationPage() {
                 <InputField placeholder="Enter password:" type="password" Icon={Lock} styling={styling.input} 
                     {...register('password', {
                         required: "Password is required", 
-                        minLength: {
-                            value: 8,
-                            message: "Password must be at least 8 characters"
-                        },
-                        maxLength: {
-                            value: 100,
-                            message: "Password must be below 100 characters"
-                        },
-                        onChange: (e: React.ChangeEvent<HTMLInputElement>) => {
-                            setPassword(e.target.value)
-                            !passwordPopup && showPasswordPopup(true)
-                        },
-                        onBlur: () => showPasswordPopup(false)
+                        validate: {
+                            hasLowerCase: (value) => /[a-z]/.test(value) || "must have a lowercase leter",
+                            hasUppercase: (value) => /[A-Z]/.test(value) || "must have an uppercase leter",
+                            hasNumber: (value) => /[0-9]/.test(value) || "must have a number",
+                            hasSymbol: (value) => /[!@#^&*()+_,.{}?-]/.test(value) || "must have a symbol (!@#^&*()+_,.{}?-)",
+                            inRange: (value) => {if (value.length < 8 || value.length > 20) return "must be between 8 and 20 characters"}
+                        }
                     })}
                 />
-                {errors.password && <p className={styling.input.error}>{errors.password.message}</p>} 
+                {errors.password && <p className={styling.input.error}>{errors.password.message}</p>}
+                {submitErrors.passwordInput && <p className={styling.input.error}>{submitErrors.passwordInput}</p>}
                 
                 <Button type="submit" text="Submit" className={styling.form.button} isLoading={isSubmitting} disabled={isSubmitting} /> 
                 <p className={styling.form.alreadyRegisteredText}>Already have an account? 
                     <Link className={styling.form.loginLink} href="/login">Login</Link>
                 </p>
+                {submitErrors.internal && <p className={styling.input.error}>{submitErrors.internal}</p>}
 
                 <SignInWithGoogleButton />
             </div>
-            
-            {passwordPopup && <PasswordChecklist password={password} />}
-                
+             
         </form>
     );
 }

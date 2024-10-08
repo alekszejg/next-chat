4c3b536995da18ca5f4625f817a27f7691663f23
Alekszej Guljajev <alexeyguljajev@gmail.com>
2024-10-09 01:57:03 +0200

    Finished types for messages, chats, chat participants (it shows Objects instead of numbers for ID to easier understand what we can obtain). Now keep doing interfaces and start adding new API routes, here permissions set for users matter since everyone is authorized but their actions might be limited, if they arent admins

--- app/chats/chat.types.ts
+++ app/chats/chat.types.ts
@@ -1,14 +1,31 @@
 import { User } from "next-auth";
 
-export interface ChatMessage {
-    id: string,
+// Anyone local can create || Anyone local can search || Owner can delete
+export interface Message {
+    id: number,
     content: string,
-    sender: User
-    sentAt: string,
+    creator_id: User, // ref users table primary key
+    created_at: string,
+    chat_id: Chat // chat contains many Message[], ref chats table primary key
 }
 
-export interface RecentChats {
+// Anyone can create || Anyone local can find || Admin(s) can edit and delete
+export interface Chat {
     id: string,
-    messages: ChatMessage[],
-    participants: User[] // for personal chats .length === 1
+    creator_id: User, // ref users table primary key
+    created_at: string,
+    title: string | null // >2 participants
+}
+
+// Anyone can create || Anyone local can find || Admin(s) can edit and delete
+export interface ChatParticipants {
+    // PRIMARY KEY (chat_id, user_id)
+    chat_id: number, // ref chats table primary key 
+    user_id: User, // ref users table primary key 
+    is_admin: boolean 
+}
+
+export interface ChatList {
+    id: number,
+    chats: Chat[]
 }

72e61884019a4fbdad7de4b90da6d83d6a10fd0d
Alekszej Guljajev <alexeyguljajev@gmail.com>
2024-10-09 01:54:01 +0200

    Modified border styling to prevent overlaps, hopefully gone

--- app/chats/chatsMenu.tsx
+++ app/chats/chatsMenu.tsx
@@ -7,10 +7,10 @@ import { Search } from "lucide-react";
 export default function ChatsMenu({ session }: {session: Session | null}) {
     
     const styling = {
-        mainWrapper: "flex flex-col w-2/5 h-full border-2 border-borderWhite",
-        profileWrapper: "flex items-center gap-x-4 h-16 pl-5 border-b-2 border-borderWhite",
+        mainWrapper: "flex flex-col w-2/5 h-full border-r-2 border-r-borderWhite",
+        profileWrapper: "flex items-center gap-x-4 h-16 pl-5 border-b-2 border-b-borderWhite",
         searchBar: {
-            wrapper: "flex items-center gap-x-3 h-12 pl-5 border-b-2 border-borderWhite",
+            wrapper: "flex items-center gap-x-3 h-12 pl-5 border-b-2 border-b-borderWhite",
             input: "w-full h-[2rem] pr-2 bg-inherit focus:text-[1.05rem] focus:outline-0 placeholder:opacity-70", 
             icon: "inline-block w-[1.2rem] opacity-60", 
             error: "text-red-500"

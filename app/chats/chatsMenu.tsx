800374872a64c747dc0f230fc130bbc1da803bd4
Alekszej Guljajev <alexeyguljajev@gmail.com>
2024-10-16 15:02:39 +0200

    misclick

--- app/chats/chatsMenu.tsx
+++ app/chats/chatsMenu.tsx
@@ -14,15 +14,18 @@ export default function ChatsMenu({ session }: {session: Session | null}) {
             input: "w-full h-[2rem] pr-2 bg-inherit focus:text-[1.05rem] focus:outline-0 placeholder:opacity-70", 
             icon: "inline-block w-[1.2rem] opacity-60", 
             error: "text-red-500"
-        }
+        } 
     };
 
     return (
         <div className={styling.mainWrapper}>
+            
             <div className={styling.profileWrapper}>
                 <MiniUserProfile session={session} />
             </div>
+            
             <InputField placeholder="Search for chat, people or groups" Icon={Search} styling={styling.searchBar} />
+
             <ChatList />
         </div>
     );

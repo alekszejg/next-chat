e6f2971c8b11a50c35d2192e98f736c0c68e0403
Alekszej Guljajev <alexeyguljajev@gmail.com>
2024-10-17 18:02:05 +0200

    Added visual button for friend removal, later develop that logic once api is nearly ready

--- app/friends/contact.tsx
+++ app/friends/contact.tsx
@@ -1,9 +1,11 @@
 import Image from "next/image";
-import { MessageSquareMore, Phone} from "lucide-react";
+import { XCircle, MessageSquareMore, Phone} from "lucide-react";
 
 export default function Contact(props: {image: string, name: string}) {
     const styling = {
-        wrapper: "flex flex-col items-center py-3 border-2 rounded-xl border-borderWhite",
+        wrapper: "flex flex-col items-center py-3 border-2 rounded-xl border-borderWhite relative",
+        removeFriendButton: "w-[1.4rem] absolute left-[6.5rem] bottom-[7rem] opacity-60 hover:scale-110 active:scale-110",
+        removeFriendIcon: "w-full h-full",
         contactImg: "rounded-full",
         nameHeader: "text-center text-lg",
         buttonWrapper: "flex gap-x-2"
@@ -11,6 +13,9 @@ export default function Contact(props: {image: string, name: string}) {
 
     return (
         <div className={styling.wrapper}>
+            <button className={styling.removeFriendButton}>
+                <XCircle className={styling.removeFriendIcon} />
+            </button>
             <Image className={styling.contactImg} src={props.image} width={50} height={50} alt={props.name} />
             <h3 className={styling.nameHeader}>{props.name}</h3>
             <div className={styling.buttonWrapper}>

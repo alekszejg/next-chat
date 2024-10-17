e6c0878f391f0445be29ef77afafd89b7df2c05a
Alekszej Guljajev <alexeyguljajev@gmail.com>
2024-10-17 18:03:04 +0200

    Now checks GET /api/user for email and name search match when input changes, might move logic into separate 'contacts' route

--- app/friends/myFriendsHeader.tsx
+++ app/friends/myFriendsHeader.tsx
@@ -1,7 +1,12 @@
+"use client"
+import { useEffect } from "react";
+import { useForm } from "react-hook-form";
+import type { Session } from "next-auth";
 import InputField from "@/app/(auth)/_components/inputField";
 import { Search, UserRoundPlus } from "lucide-react";
 
-export default function myFriendsHeader() {
+
+export default function myFriendsHeader(props: {session: Session | null}) {
     const styling = {
         header: "pl-6 text-2xl",
         peopleSearchWrapper: {
@@ -17,14 +22,40 @@ export default function myFriendsHeader() {
         }
     };
 
+    const { register, watch } = useForm({mode: "onChange"});
+
+    const userSearch = watch("userSearch");
+    
+    useEffect(() => {
+        if (!userSearch) return;
+        const searchFriend = async () => {
+            const input = encodeURIComponent(userSearch);
+            const url =  `http://localhost:3000/api/user?user=${props.session?.id}&search-for=friend&input=${input}`;
+            const response = await fetch(url, {
+                method: "GET",
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+            })
+            const data = response.json();
+            console.log(data)
+        }
+        searchFriend();
+        
+    }, [userSearch])
+
     return (
         <>
         <h2 className={styling.header}>My Friends</h2>
         <div className={styling.peopleSearchWrapper.wrapper}>
+            
             <button className={styling.peopleSearchWrapper.addFriendButton}>
                 <UserRoundPlus className={styling.peopleSearchWrapper.addFriendIcon} />
             </button>
-            <InputField placeholder="Find a friend" Icon={Search} styling={styling.peopleSearchWrapper.friendSearch} />
+            
+            <InputField placeholder="Find a friend" Icon={Search} styling={styling.peopleSearchWrapper.friendSearch} 
+                {...register('userSearch', {})}
+            />
         </div>
         </>
     );

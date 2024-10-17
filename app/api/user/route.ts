bddbaf1a54e76b9226fe60e3afe3e8892a35283d
Alekszej Guljajev <alexeyguljajev@gmail.com>
2024-10-17 18:01:37 +0200

    adjusted GET /api/user route to allow search for users contacts. Got search by name and email working but might move logic into separate route later. Also authorization will have to be fixed/improved

--- app/api/user/route.ts
+++ app/api/user/route.ts
@@ -5,14 +5,43 @@ import type { PoolClient } from 'pg';
 import addNewUser from '@/app/_actions/addNewUser';
 
 export async function GET(req: NextRequest) {
-    const authHeader = req.headers.get('Authorization');
+    //const authHeader = req.headers.get('Authorization');
     const searchParams = req.nextUrl.searchParams;
+    const userID = searchParams.get("user");
+    const searchFor = searchParams.get("search-for");
+    const input = searchParams.get("input");
     const email = searchParams.get('email');
+    
     let client: PoolClient | null = null;
    
-    if (!authHeader || !authHeader.startsWith('Bearer ') || authHeader.split(' ')[1] !== process.env.API_KEY) {
+    /*if (!authHeader || !authHeader.startsWith('Bearer ') || authHeader.split(' ')[1] !== process.env.API_KEY) {
         console.error("Get route received no authorization")
         return NextResponse.json({ error: 'Unauthorized' }, { status: 401 });
+    }
+    */
+
+    if (searchFor === "friend") {
+        if (userID && input) {
+            let client: PoolClient | null = null;
+            try {
+                client = await ConnectPgsqlPool("connect");
+                // ensure friendship query works in both ways (not jsut pending friend requests)
+                const query = `SELECT u.id, u.name, u.email FROM users AS u 
+                JOIN contacts AS c1 ON (c1.user_id = $1 AND c1.contact_id = u.id)
+                JOIN contacts AS c2 ON (c2.user_id = u.id AND c2.contact_id = $1)
+                WHERE (u.email LIKE $2 OR u.name LIKE $2);`;
+                
+                if (!client) return NextResponse.json({error: "Connection to DB failed"}, {status: 500});
+                console.log("got the client")
+                const response = await client.query(query, [userID, `${input}%`]);
+                ConnectPgsqlPool("disconnect", client);
+                if (response.rows.length === 0) return NextResponse.json({success: false}, {status: 200});
+                return NextResponse.json({success: true, matches: response.rows}, {status: 200})
+            } catch {
+                if (client) ConnectPgsqlPool("disconnect", client);
+                return NextResponse.json({error: "Either db connection or query failed"}, {status: 500});
+            } 
+        }
     }
 
     if (!email) {

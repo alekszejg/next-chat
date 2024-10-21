93f7b4978e0b09647cf6ce5af9c8dcac521dab0c
Alekszej Guljajev <alexeyguljajev@gmail.com>
2024-10-21 18:40:51 +0200

    Edited props for ChatsMenu where it receives components with server-side data like chatlist (contactlist is still client-side to enable selection of multiple contacts for group chats, later check if its possible to move down the client-side)

--- app/chats/page.tsx
+++ app/chats/page.tsx
@@ -1,17 +1,24 @@
 import { auth } from "@/auth";
 import type { Session } from "next-auth";
 import ChatsMenu from "@/app/chats/chatsMenu";
+import ChatList from "@/app/chats/chatList";
+import ContactList from "@/app/chats/contactsList";
+import Chat from "@/app/chats/chat";
 
-export default async function ChatsPage() {
+
+export default async function ChatsPage({ searchParams }: {searchParams?: {chatID?: string}}) {
     const session: Session | null = await auth();
-    
+    console.log("chatid is: ", searchParams?.chatID)
     const styling = {
-        wrapper: "flex flex-col"
+        wrapper: "flex"
     }
 
     return (
         <div className={styling.wrapper}>
-            <ChatsMenu session={session} />
+            <ChatsMenu 
+                session={session} 
+                ContactList={<ContactList session={session}/>} 
+                ChatList={<ChatList />}/>
         </div>
     );
 }

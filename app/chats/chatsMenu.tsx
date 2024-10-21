531ca585730c8d1481861fe0a30acc484c0ce9bc
Alekszej Guljajev <alexeyguljajev@gmail.com>
2024-10-21 18:38:24 +0200

    ChatsMenu component is now client side, which enables to monitor chat searchbar and create new chats with users

--- app/chats/chatsMenu.tsx
+++ app/chats/chatsMenu.tsx
@@ -1,32 +1,50 @@
+"use client"
+import { useState } from "react";
 import type { Session } from "next-auth";
 import MiniUserProfile from "@/app/chats/miniUserProfile";
 import InputField from "@/app/(auth)/_components/inputField";
-import { Search } from "lucide-react";
-import ChatList from "./chatList";
+import { Search, Circle, CircleCheck, MessageSquarePlus } from "lucide-react";
 
-export default function ChatsMenu({ session }: {session: Session | null}) {
-    
+
+export default function ChatsMenu(props: {session: Session | null, ContactList: React.ReactNode, ChatList: React.ReactNode}) {
+    const [showContacts, setShowContacts] = useState(false);
+    const toggleContacts = () => setShowContacts(!showContacts);
+  
     const styling = {
-        mainWrapper: "flex flex-col w-2/5 h-full border-r-2 border-r-borderWhite",
+        mainWrapper: "flex flex-col w-2/5 h-full border-r-2 border-r-borderWhite relative",
         profileWrapper: "flex items-center gap-x-4 h-16 pl-5 border-b-2 border-b-borderWhite",
+        searchBarWrapper: "flex items-center pr-3 border-b-2 border-b-borderWhite",
         searchBar: {
-            wrapper: "flex items-center gap-x-3 h-12 pl-5 border-b-2 border-b-borderWhite",
+            wrapper: "flex items-center gap-x-3 h-12 pl-5",
             input: "w-full h-[2rem] pr-2 bg-inherit focus:text-[1.05rem] focus:outline-0 placeholder:opacity-70", 
             icon: "inline-block w-[1.2rem] opacity-60", 
             error: "text-red-500"
-        } 
+        },
+        addChatIcon: showContacts ? "w-[1.5rem]" : "w-[1.5rem] opacity-60",
+        contactsWrapper: showContacts ? "block w-full" : "hidden"
     };
 
+
+    
     return (
         <div className={styling.mainWrapper}>
             
             <div className={styling.profileWrapper}>
-                <MiniUserProfile session={session} />
+                <MiniUserProfile session={props.session} />
             </div>
             
-            <InputField placeholder="Search for chat, people or groups" Icon={Search} styling={styling.searchBar} />
+            <div className={styling.searchBarWrapper}>
+                <InputField placeholder="Search for chat, people or groups" Icon={Search} styling={styling.searchBar} />
+                <MessageSquarePlus className={styling.addChatIcon} onClick={toggleContacts} />
+            </div>
 
-            <ChatList />
+            {showContacts && 
+                <section className={styling.contactsWrapper}>
+                    {props.ContactList}
+                </section>
+            }
+
+            {props.ChatList}
         </div>
     );
 }

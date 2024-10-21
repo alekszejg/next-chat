54d8fc41198d09ed36011ea9d0eb9848d138a56f
Alekszej Guljajev <alexeyguljajev@gmail.com>
2024-10-21 18:45:21 +0200

    Added ContactList wwith some data fetching and ability to select multiple users for a groupchat. Still needs logic for sending POST /api/chats request for creating individual and group chats. Later see if it's possible to move some state down or split it into smaller components to obtain server-side data fetching

---
+++ app/chats/contactsList.tsx
@@ -0,0 +1,78 @@
+"use client"
+import { Session } from "next-auth";
+import { useState, useEffect } from "react";
+import type { Contact } from "@/app/friends/contactsInfo";
+import ChatContact from "@/app/chats/chatContact";
+
+
+export default function ContactList({ session }: {session: Session | null}) {
+    const styling = {
+        createGroupChatButton: "mt-3 ml-3",
+        contactWrapper: "flex items-center gap-x-3 h-14 pl-3 border-b-2 border-b-borderWhite",
+        addFriends: "mt-5 text-center opacity-60",
+        internalError: "text-center opacity-60"
+    };
+
+    let internalError = false;
+    const [contactList, setContactList] = useState<Contact[]>([]);
+    
+    useEffect(() => {
+        const fetchContacts = async () => {
+            try {
+                const url = `http://localhost:3000/api/contacts?user=${session?.id}&status=friends`;
+                const response = await fetch(url, {
+                    method: "GET",
+                    headers: {
+                        'Content-Type': 'application/json',
+                    },
+                });
+
+                if (response.ok) {
+                    const data = await response.json();
+                    data.match && setContactList(data.matches);
+                }
+                
+                if (!response.ok) {
+                    internalError = true;
+                }
+
+            } catch {
+                internalError = true;
+                console.error(`Failed to fetch contacts for user ${session?.id}`)
+            }
+        }
+        fetchContacts();
+    }, []);
+
+    const [selectedUsers, setSelectedUsers] = useState<string[]>([]);
+    
+    const handleUserSelection = (mode: "add" | "remove", userID: string) => {
+        setSelectedUsers((prevSelectedUsers) => {
+            if (mode === "add") return [...prevSelectedUsers, userID]
+            return prevSelectedUsers.filter(id => id !== userID);
+        })
+    }
+
+    return (
+        <>
+        {internalError && 
+            <p className={styling.internalError}>Couldn't load the friend list. Try again later</p>
+        }
+
+        {!internalError && contactList.length === 0 &&
+            <p className={styling.addFriends}>Add more friends to chat with</p>
+        }
+
+        {!internalError && contactList.length > 0 && 
+            <>
+            <button className={styling.createGroupChatButton}>Create a groupchat ({selectedUsers.length} selected)</button>
+            {contactList.map(contact => (
+                <ChatContact key={contact.id} info={contact} wrapperStyling={styling.contactWrapper} handleUserSelection={handleUserSelection}/>
+            ))}
+            </>
+        }
+        </>
+        
+    );
+
+}

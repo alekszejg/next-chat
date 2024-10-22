116a68b9072f0cb2a5b13f679e636865f534701b
Alekszej Guljajev <alexeyguljajev@gmail.com>
2024-10-22 23:24:32 +0200

    Started messages fetching logic once chatID is given

--- app/chats/chat.tsx
+++ app/chats/chat.tsx
@@ -1,10 +1,28 @@
 "use client"
 import { useEffect } from "react";
 import { socket } from "@/app/socket";
+import type { Session } from "next-auth";
 
-export default function Chat() {
+
+export default function Chat({ session, chatID }: {session: Session | null, chatID: string | null}) {
+    const styling = {
+      wrapper: chatID ? "block" : "hidden"
+    }
+
+    const getChatInfo = async () => {
+      const url = `http://localhost:3000/api/chats?userID=${session?.id}&chatID=${chatID}`
+      const response = await fetch(url, {
+        method: "GET",
+        headers: {
+          'Content-Type': 'application/json',
+        }
+      })
+    }
+
+
     useEffect(() => {
-        // Connect the socket when the component mounts
+        if (!chatID) return;
+        
         socket.connect();
     
         socket.on("connect", () => {
@@ -20,6 +38,6 @@ export default function Chat() {
     }, []);
 
     return (
-        <div>This is the chat section</div>
+        <div className={styling.wrapper}>This is the chat section</div>
     )
 }

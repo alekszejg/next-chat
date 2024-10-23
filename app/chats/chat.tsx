9b4547d69da735dea55e73784cb45c8c8a45e076
Alekszej Guljajev <alexeyguljajev@gmail.com>
2024-10-23 19:06:21 +0200

    Added async function to obtain info from GET /api/chats along with some user friendly error handling and case of no messages being found. Now need to make an interface and ensure sockets do their job. Logic looks good so far! Needs to be less long and less complicated tho...

--- app/chats/chat.tsx
+++ app/chats/chat.tsx
@@ -1,13 +1,23 @@
 "use client"
-import { useEffect } from "react";
+import { useState, useEffect } from "react";
 import { socket } from "@/app/socket";
 import type { Session } from "next-auth";
+import type { Message } from "@/app/chats/chat.types";
 
+type User = {id: string, name: string, email: string, image: string};
+type UserInfo = {name: string, email: string, image: string}
 
 export default function Chat({ session, chatID }: {session: Session | null, chatID: string | null}) {
     const styling = {
-      wrapper: chatID ? "block" : "hidden"
+      wrapper: chatID ? "block" : "hidden",
+      noMessages: "text-sm, text-center, opacity-60",
+      error: "text-sm, text-center, opacity-60"
     }
+
+    const [error, setError] = useState<null | string>(null);
+    let userInfo: {[key: string]: UserInfo} = {};
+    let messages: Message[] | [] = [];
+    
 
     const getChatInfo = async () => {
       const url = `http://localhost:3000/api/chats?userID=${session?.id}&chatID=${chatID}`
@@ -17,12 +27,22 @@ export default function Chat({ session, chatID }: {session: Session | null, chat
           'Content-Type': 'application/json',
         }
       })
+      if (response.ok) {
+        const data = await response.json();
+        data.users.map((user: User) => {
+          userInfo[user.id] = {name: user.name, email: user.email, image: user.image};
+        });
+        messages = data.messages;
+      } else {
+          setError("Couldn't load chat information. Try again later");
+      }
     }
 
 
     useEffect(() => {
         if (!chatID) return;
-        
+        getChatInfo();
+
         socket.connect();
     
         socket.on("connect", () => {
@@ -38,6 +58,15 @@ export default function Chat({ session, chatID }: {session: Session | null, chat
     }, []);
 
     return (
-        <div className={styling.wrapper}>This is the chat section</div>
+        <div className={styling.wrapper}>
+          
+          {error && <p className={styling.error}>{error}</p>}
+          
+          {!error && messages.length === 0 && 
+            <p className={styling.noMessages}>
+              Messages will appear here
+            </p>
+          }       
+        </div>
     )
 }
